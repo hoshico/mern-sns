cfdebf3b57a2f8c7bbba68abc0ee4a8e2fa4553f
hoshico <melancholinista.yasu@gmail.com>
2022-07-02 16:05:35 +0900

    add login

--- routes/auth.js
+++ routes/auth.js
@@ -20,5 +20,26 @@ router.post("/register", async (req, res) => {
   }
 });
 
+/*
+  ログイン
+*/
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) return res.status(404).send("ユーザーが見つかりません");
+    /*
+      入力されたパスとuser(emailからの検索)のパスが一致すれば 
+      valledPasswordにはtrueが入る
+    */
+    const vailedPassword = req.body.password === user.password;
+    if(!vailedPassword) return res.status(400).json("パスワードが違います");
+
+    // ifに引っ掛からなかった場合は200番を返す
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 // exportする必要がある
 module.exports = router;
