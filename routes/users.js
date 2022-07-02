eda4a02843b6f6de2b7dacf0325a75e25d33d50c
hoshico <melancholinista.yasu@gmail.com>
2022-07-02 17:39:50 +0900

    add upload userinformation func

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
+/*
+  CRUD
+  ユーザー情報の更新
+*/
+router.put("/:id", async(req, res) => {
+  if(req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("ユーザー情報が更新されました");
+    } catch(err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("あなたは自分のアカウントの時だけ更新できます");
+  }
+});
 
-//router.get('/', (req, res) => {
-//  res.send("user router");
-//});
 
 // exportする必要がある
 module.exports = router;
